52f4b0486428b7e78f2c0e18af2af7c05ffc9650
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-02-12 13:37:10 +0100

    Added signup serializer

---
+++ rental/serializers/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""contains authentication related serializers"""
+
+from rest_framework import serializers
+from rental.models.user import MainUser
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    """
+    Signup serializer
+    """
+
+    class Meta:
+        model = MainUser
+        fields = ('email', 'password', 'first_name', 'last_name', 'role')
