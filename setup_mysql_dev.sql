e1bf8fea8a35bff323b9fbf2dba6b38179530078
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-03-13 10:16:11 +0100

    Little updates

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 
 
 CREATE DATABASE IF NOT EXISTS rentease_db;
-       CREATE USER IF NOT EXISTS 'rent_user'@'localhost' IDENTIFIED BY 'rent_pwd';
+       CREATE USER IF NOT EXISTS 'rent_user'@'localhost' IDENTIFIED BY 'Rent_pwd123@';
               GRANT ALL PRIVILEGES ON rentease_db.* TO 'rent_user'@'localhost';
                                       GRANT SELECT ON performance_schema.* TO 'rent_user'@'localhost';
 FLUSH PRIVILEGES;
